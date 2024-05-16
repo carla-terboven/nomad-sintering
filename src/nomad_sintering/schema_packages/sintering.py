241833c3d7be9957ca615f8245f0ff01eab34dba
carla-terboven <carla.terboven@helmholtz-berlin.de>
2024-05-16 09:47:51 +0200

    Added a normalize function to the Sintering schema

--- src/nomad_sintering/schema_packages/sintering.py
+++ src/nomad_sintering/schema_packages/sintering.py
@@ -16,6 +16,9 @@
 # limitations under the License.
 #
 
+from nomad.units import ureg
+import pandas as pd
+
 from nomad.datamodel.data import EntryData
 from nomad.datamodel.metainfo.basesections import Process
 from nomad.datamodel.metainfo.basesections import ProcessStep
@@ -101,6 +104,13 @@ class Sintering(Process, EntryData, ArchiveSection):
         section_def=TemperatureRamp,
         repeats=True,
     )
+    data_file = Quantity(
+        type=str,
+        description='The recipe file for the sintering process.',
+        a_eln={
+            "component": "FileEditQuantity",
+        },
+    )
 
     def normalize(self, archive: 'EntryArchive', logger: 'BoundLogger') -> None:
         '''
@@ -112,6 +122,17 @@ class Sintering(Process, EntryData, ArchiveSection):
             logger (BoundLogger): A structlog logger.
         '''
         super().normalize(archive, logger)
-
+        if self.data_file:
+            with archive.m_context.raw_file(self.data_file) as file:
+                df = pd.read_csv(file)
+            steps = []
+            for i, row in df.iterrows():
+                step = TemperatureRamp()
+                step.name = row['step name']
+                step.duration = ureg.Quantity(float(row['duration [min]']), 'minutes')
+                step.initial_temperature = ureg.Quantity(row['initial temperature [C]'], 'celsius')
+                step.final_temperature = ureg.Quantity(row['final temperature [C]'], 'celsius')
+                steps.append(step)
+        self.steps = steps
 
 m_package.__init_metainfo__()
